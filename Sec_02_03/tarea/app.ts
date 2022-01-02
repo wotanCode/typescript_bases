2c616200e1b526bdce58c3efca3646d3b56af789
Pedro Yanez <pedro_y91@outlook.com>
2022-01-02 19:31:08 -0300

    25/tarea

--- Sec_02_03/tarea/app.ts
+++ Sec_02_03/tarea/app.ts
@@ -1,36 +1,42 @@
 (() => {
 
     // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
+    const batman:string  = 'Bruce';
+    const superman:string = 'Clark';
   
-    const existe = false;
+    const existe:boolean = false;
   
     // Tuplas
-    const parejaHeroes = [batman,superman];
-    const villano = ['Lex Lutor',5,true];
+    const parejaHeroes:[string,string] = [batman,superman];
+    const villano:[string,number,boolean] = ['Lex Lutor',5,true];
   
     // Arreglos
-    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
   
     //Enumeraciones
-    const fuerzaFlash = 5;
-    const fuerzaSuperman = 100;
-    const fuerzaBatman = 1;
-    const fuerzaAcuaman = 0;
+    enum fuerza {
+      acuaman = 0,
+      batman = 1,
+      flash = 5,
+      superman = 100,
+    }
+    const fuerzaFlash:fuerza = fuerza.flash;
+    const fuerzaSuperman:fuerza = fuerza.superman;
+    const fuerzaBatman:fuerza = fuerza.batman;
+    const fuerzaAcuaman:fuerza = fuerza.acuaman;
   
     // Retorno de funciones
-    function activar_batiseñal(){
+    function activar_batiseñal():string{
       return 'activada';
     }
   
-    function pedir_ayuda(){
+    function pedir_ayuda():void{
       console.log('Auxilio!!!');
     }
   
     // Aserciones de Tipo
     const poder: any = '100';
-    const largoDelPoder:number = poder.length;
+    const largoDelPoder:number = (poder as string).length;
     console.log( largoDelPoder );
   
   
