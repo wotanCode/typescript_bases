f73bded1f2855e5135ae611edeb5276f30976b6c
Pedro Yanez <pedro_y91@outlook.com>
2022-01-02 10:51:53 -0300

    sec2/modo observador

--- Seccion_2/app.js
+++ Seccion_2/app.js
@@ -1,7 +1,6 @@
-const msg: string = 'hola mundo';
-console.log(msg)
+"use strict";
+const msg = 'hola mundo';
+console.log(msg);
 //tsc app = transpila el codigo de ts a javascript
-
 //tsc --init = Esto inicia el tsconfig para configuraciones propias de typescript
-
 //tsc -w O tsc --watch = Iniciammos el modo observador
