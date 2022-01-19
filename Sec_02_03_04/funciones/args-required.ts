163cc0b4eab5824c907da5e7d659dfc841c6a7af
Pedro Yanez <pedro_y91@outlook.com>
2022-01-19 11:56:05 -0300

    28/Parametros obligatorios de las funciones

---
+++ Sec_02_03_04/funciones/args-required.ts
@@ -0,0 +1,11 @@
+(()=>{
+
+  const fullName = ( firstName: string, lastName:string ): string => {
+    return `${ firstName } ${ lastName }`;
+  }
+
+  const name = fullName('Tony', 'Startk');
+
+  console.log({ name })
+
+})()
