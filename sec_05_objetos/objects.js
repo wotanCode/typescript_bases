b91ad77926cef03befc7f9609afef965705ecd2b
Pedro Yanez <pedro_y91@outlook.com>
2022-02-11 12:23:26 -0300

    35/objetos basicos

---
+++ sec_05_objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        name: 'Otro nombre',
+        age: 60,
+        powers: ['Super Fuerza'],
+    };
+})();
---
+++ sec_05_objetos/objects.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+  let flash= {
+    name: 'Barry Alen',
+    age: 24,
+    powers: ['Super velocidad','Viajar en el tiempo']
+  }
+
+  flash = {
+    name: 'Otro nombre',
+    age: 60,
+    powers: ['Super Fuerza'],
+  }
+
+})()
