036160f479e5fd587694e16cb0e8b128e32f1664
Pedro Yanez <pedro_y91@outlook.com>
2022-02-15 17:39:23 -0300

    39/tipos personalizados

---
+++ sec_05_objetos/type_01.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: [1, 2]
+    };
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1]
+    };
+    console.log(flash);
+    console.log(superman);
+})();
