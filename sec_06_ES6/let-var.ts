70b38eb3503b5022780368aa266e088724373805
Pedro Yanez <pedro_y91@outlook.com>
2022-02-16 11:38:12 -0300

    51/variables-let

---
+++ sec_06_ES6/let-var.ts
@@ -0,0 +1,20 @@
+(()=>{
+
+
+  //Var NO ES ADMITIDO EL VAR
+
+  let a:string = "SOY UN LET";
+  const b:string = "soy una constante";
+  console.log(a)
+  console.log(b)
+
+  const numbers = {
+    a: 1,
+    b: 2,
+  };
+
+  numbers.a = 100,
+
+  console.log(numbers.a)
+  
+})()
