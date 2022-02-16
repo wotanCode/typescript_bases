35e88f8767e24e7d5dfa892fbc483fcc739adceb
Pedro Yanez <pedro_y91@outlook.com>
2022-02-16 16:00:17 -0300

    wip

--- sec_07_classes/basica.ts
+++ sec_07_classes/basica.ts
@@ -6,7 +6,7 @@
     public realName?: string;
     static avgAge: number = 35;
 
-    constructor (name:string, team: string, realName?: string){
+    constructor ( name:string, team: string, realName?: string){
       this.name = name;
       this.team = team;
       this.realName = realName;
