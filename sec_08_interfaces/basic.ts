bec6229ca8d5c7e600fb5e29d2585b1b41988e29
Pedro Yanez <pedro_y91@outlook.com>
2022-02-17 13:08:19 -0300

    67/interfaz basica

---
+++ sec_08_interfaces/basic.ts
@@ -0,0 +1,21 @@
+(()=>{
+
+  interface Hero {
+    name: string;
+    age?: number;
+    powers: number[];
+    getName?: ()=> string;
+  }
+
+    let flash: Hero = {
+        name: 'Barry Alen',
+        age: 24,
+        powers: [1,2]
+    };
+    let superman: Hero = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1]
+    };
+
+})()
