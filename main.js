2baa9390e91c2781c100007db72b1134af6ec76b
Pedro Yanez <pedro_y91@outlook.com>
2022-02-17 08:35:43 -0300

    60/metodos publicos y privados

--- main.js
+++ main.js
@@ -27,4 +27,23 @@
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
 })();
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})!!`;
+        }
+    }
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
+    console.log(antman);
+    console.log(antman.bio());
+})();
 //# sourceMappingURL=main.js.map
