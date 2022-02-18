8a450b1209f67dc9d0d4db39a8ed628fbff70811
Pedro Yanez <pedro_y91@outlook.com>
2022-02-18 13:31:33 -0300

    83/intro a los genericos

---
+++ Advance/src/generics/generics.ts
@@ -0,0 +1,7 @@
+export const printObject = (argument:any) => {
+  console.log(argument);
+}
+
+export function genericFunction (argument: any){
+  return argument
+}
--- Advance/src/index.ts
+++ Advance/src/index.ts
@@ -1,14 +1,19 @@
-import { Hero } from './classes/hero'
+//import { Hero } from './classes/hero'
 //import { Hero as Superhero } from './classes/hero'
 //import * as HeroClasses from './classes/hero'
 //import { powers } from './data/powers'
-
+import { printObject, genericFunction } from './generics/generics';
 //const Hero = 123;
+// const ironman = new Hero('Ironman',1,40);
+// console.log(ironman);
+// //console.log( powers);
+// console.log(ironman.power)
 
-const ironman = new Hero('Ironman',1,40);
 
-console.log(ironman);
+printObject("pedrot");
+printObject(new Date());
+printObject(123);
+printObject([1,2,3,4,5]);
 
-//console.log( powers);
-
-console.log(ironman.power)
+console.log(genericFunction(3.1412).toFixed(2));
+console.log(genericFunction("holasss"));
