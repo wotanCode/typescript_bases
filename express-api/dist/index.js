debfafdf89fb5d1b0d948159c6f027c9a30a6312
Pedro Yanez <pedro_y91@outlook.com>
2022-02-21 09:21:44 -0300

    102/trabajar con ts en lugar de js

---
+++ express-api/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+//const express = require('express')
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.status(201).json({
+        ok: true,
+        msg: 'OK token en la peticion'
+    });
+    res.json({
+        ok: true,
+        msg: "todo salio bien",
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- express-api/index.ts
+++ express-api/index.ts
@@ -1,13 +1,22 @@
-const express = require('express')
+import express from 'express'
+//const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  res.status(201).json({
+    ok: true,
+    msg:'OK token en la peticion'
+  });
+
   res.json({
     ok: true,
     msg: "todo salio bien",
   })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
