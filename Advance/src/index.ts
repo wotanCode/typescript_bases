f6c4a360213f4502005aae1c80b2734bb87d0cb2
Pedro Yanez <pedro_y91@outlook.com>
2022-02-21 07:57:59 -0300

    93/decoradores de clase

--- Advance/src/index.ts
+++ Advance/src/index.ts
@@ -1,7 +1,10 @@
-import { getPokemon } from "./generics/get-pokemons";
+//// import { getPokemon } from "./generics/get-pokemons";
+import { Pokemon } from './decorators.ts/pokemon-class';
+// getPokemon(4)
+//   .then( pokemon => console.log(pokemon.sprites.front_default))
+//   .catch( error => console.error( error ))
+//   .finally( () => console.log('FIN del index'))
 
+const charmander = new Pokemon('Charmander');
 
-getPokemon(4)
-  .then( pokemon => console.log(pokemon.sprites.front_default))
-  .catch( error => console.error( error ))
-  .finally( () => console.log('FIN del index'))
+console.log(charmander);
