aa78347ed55d0a0226b0e95e695d37893b851a2f
Pedro Yanez <pedro_y91@outlook.com>
2022-02-21 09:05:34 -0300

    101/creando rest api

--- express-api/index.js
+++ express-api/index.js
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: "todo salio bien",
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
