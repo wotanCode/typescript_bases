1e4b1dcc5da1c5ed8df97ebca068f5e673ed0a67
Pedro Yanez <pedro_y91@outlook.com>
2022-02-21 06:56:48 -0300

    86/ agrupar genericcos

---
+++ Advance/src/backs/generics.ts
@@ -0,0 +1,31 @@
+//import { Hero } from './classes/hero'
+//import { Hero as Superhero } from './classes/hero'
+//import * as HeroClasses from './classes/hero'
+//import { powers } from './data/powers'
+// import { HeroI } from './interfaces/hero';
+// import { VillanI } from './interfaces/villan';
+import { VillanI, HeroI } from '../interfaces';
+import { printObject, genericFunction, genericFunctionArrow } from '../generics/generics';
+//const Hero = 123;
+// const ironman = new Hero('Ironman',1,40);
+// console.log(ironman);
+// //console.log( powers);
+// console.log(ironman.power)
+
+
+// printObject("pedrot");
+// printObject(new Date());
+// printObject(123);
+// printObject([1,2,3,4,5]);
+
+// console.log(genericFunctionArrow(3.1412).toFixed(2));
+// console.log(genericFunction("holasss").toUpperCase());
+// console.log(genericFunction(new Date()).getDate());
+
+const deadpool = {
+  name: 'DeadPool',
+  realName: 'Wade Winston Wilson',
+  dangerLevel: 130,
+}
+
+console.log( genericFunctionArrow<VillanI>(deadpool).dangerLevel )
