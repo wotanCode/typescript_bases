90554889e8c9a42cfeb2c6a117f944b2c8428420
Pedro Yanez <pedro_y91@outlook.com>
2022-02-21 07:36:11 -0300

    88/Mapear respuestas HTTP

--- Advance/src/generics/get-pokemons.ts
+++ Advance/src/generics/get-pokemons.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { PokemonI } from '../interfaces/pokemon';
 
-export const getPokemon = async( pokemonId: number) =>{
+export const getPokemon = async( pokemonId: number): Promise<PokemonI> =>{
 
-  const resp = await axios.get<string>(`https://pokeapi.co/api/v2/pokemon/${ pokemonId }`)
-  console.log(resp)
+  const {data} = await axios.get<PokemonI>(`https://pokeapi.co/api/v2/pokemon/${ pokemonId }`)
+  //console.log( resp.data.abilities[0] )
 
   // console.log('hola pokemon');
   // if (true) throw new Error ('auxilio!');
 
-  return 1;
+  return data;
 }
--- Advance/src/index.ts
+++ Advance/src/index.ts
@@ -1,6 +1,7 @@
 import { getPokemon } from "./generics/get-pokemons";
 
+
 getPokemon(4)
-  .then( resp => console.log(resp))
+  .then( pokemon => console.log(pokemon.sprites.front_default))
   .catch( error => console.error( error ))
-  .finally( () => console.log('fin del get pokemon'))
+  .finally( () => console.log('FIN del index'))
